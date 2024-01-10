00f9d4997166495e57b312cadfda122ce90f691d
bhrsto <135649773+bhrsto@users.noreply.github.com>
2024-01-10 15:54:19 +0100

    Update GIzvjestaj.h

--- GIzvjestaj.h
+++ GIzvjestaj.h
@@ -12,15 +12,9 @@
 class Izvjestaj {
 	private:
 		String naslov;
-		//String sadrzaj;
 		TMemo *MemoIspis;
 
 	public:
-	   /*	wchar_t ime[20], prezime[20], nazivKnjige[40];
-		int id;
-		String ulaz;
-		int zeljenaDuljina; */
-
 		Izvjestaj(String naslov);
 		Izvjestaj(TMemo *MemoIspis);
 
